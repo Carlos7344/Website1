e206b12069724f750f41dab25d9e143142a51cb8
Carlos Ruvalcaba <142750480+Carlos7344@users.noreply.github.com>
2024-02-28 18:08:18 +0000

    Wed, Feb 28, 2024, 10:08 AM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -7,7 +7,7 @@ panels.forEach(panel => {
     })
 })
 
-fucntion removeActiveClasses(){
+function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
