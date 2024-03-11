cceb69a393772226c44ae05a3c44fba1864ea2b8
Carlos Ruvalcaba <142750480+Carlos7344@users.noreply.github.com>
2024-03-11 17:06:15 +0000

    Mon, Mar 11, 2024, 10:06 AM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -3,15 +3,24 @@ open = document.getElementById('open')
 modal = document.getElementById('modal')
 close = document.getElementByID('close')
 
-
+// Toggle Nav
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+//Show Modal
 opne.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+//Hide Modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+//Hide Modal on outside click
+window.addEventListener('click', () =>{
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
