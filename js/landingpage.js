1525fbf2e1a30002cfd7bdffad5fb9e4f9dfc61a
Carlos Ruvalcaba <142750480+Carlos7344@users.noreply.github.com>
2024-03-11 16:56:41 +0000

    Mon, Mar 11, 2024, 9:56 AM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,5 +1,11 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+opne.addEventListener('click', () => {
+    modal.classList.add('show-modal')
 })
